955fe843a19940071e6c7a359473641f538d3c1e
Scott <swsteffes@gmail.com>
2019-11-18 22:41:15 +0900

    Initial Commit

---
+++ Assets/SceneLoader.cs
@@ -0,0 +1,36 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+public class SceneLoader
+{
+    [MenuItem("Scenes/Load TitleScene")]
+    public static void LoadTitleScene() { openScene(Scenes.TitleScene); }
+
+    [MenuItem("Scenes/Load GameScene1")]
+    public static void LoadGameScene1() { openScene(Scenes.GameScene1); }
+
+    [MenuItem("Scenes/Load GameScene2")]
+    public static void LoadGameScene2() { openScene(Scenes.GameScene2); }
+
+    [MenuItem("Scenes/Load GameScene3")]
+    public static void LoadGameScene4() { openScene(Scenes.GameScene3); }
+
+    [MenuItem("Scenes/Load EndScene")]
+    public static void LoadEndScene() { openScene(Scenes.EndScene); }
+
+    private const string PATH = "Assets/scenes/";
+    private const string SCENE_SUFFIX = ".unity";
+
+    private static void openScene(Scenes scene)
+    {
+        if (EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())
+            EditorSceneManager.OpenScene(GetScenePath(scene.Name()), OpenSceneMode.Single);
+    }
+
+    private static string GetScenePath(string sceneName)
+    {
+        return PATH + sceneName + SCENE_SUFFIX;
+    }
+}
+#endif
---
+++ Assets/Scenes.cs
@@ -0,0 +1,30 @@
+﻿public enum Scenes
+{
+    TitleScene,
+    GameScene1,
+    GameScene2,
+    GameScene3,
+    EndScene
+}
+
+public static class ScenesExtensions
+{
+    public static string Name(this Scenes scene)
+    {
+        switch (scene)
+        {
+            case Scenes.TitleScene:
+                return "TitleScene";
+            case Scenes.GameScene1:
+                return "GameScene1";
+            case Scenes.GameScene2:
+                return "GameScene2";
+            case Scenes.GameScene3:
+                return "GameScene3";
+            case Scenes.EndScene:
+                return "EndScene";
+            default:
+                return "Scene Not Found";
+        }
+    }
+}
